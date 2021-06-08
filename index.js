62df4ed352bdc904cda6ae4e286aeaeaa844a1d7
will14lee <will14lee@gmail.com>
2021-06-08 01:14:58 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
-// Write your code here
+const num1=31;
+const num2=2;
+
+const multiply=num1*num2;
+
+const random=(Math.floor(Math.random()*10)+1);
+const isInt=Number.isInteger(random);
+console.log(random);
+console.log(isInt)
+
+const num3=16
+const num4=6
+const mod= num3 % num4
+console.log(mod)
+
+
+const max=Math.max(1,4,6,20,19,16,9)
+console.log(max)
+/*  describe('max', function() {
+    it("will return 20 as the highest number in the set", function() {
+      expect(max).to.eq(20)
+    });
+  });
+});
+*/
